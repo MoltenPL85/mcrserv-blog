47180ad61101d9b3e735834c85e42b8fec805a1f
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-04-30 22:03:18 +0500

    Query service for fetching data

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,21 +1,4 @@
-import axios from 'axios';
-import { useEffect, useState, useCallback } from 'react';
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = useCallback(async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  }, [postId]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
